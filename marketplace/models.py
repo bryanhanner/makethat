59b56fcbdaad0625a1bcb8dc179fb9fec02a528a
Miranda Ma <majiayi0319@gmail.com>
2022-02-14 09:39:55 -0800

    more django

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Creator(models.Model):
+    name = models.CharField(max_length=50)
+    join_date = models.DateTimeField('date joined')
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    creator = models.ForeignKey(Creator, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    price = models.IntegerField(default=0)
+    content = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
